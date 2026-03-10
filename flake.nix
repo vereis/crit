a930e355cc2af9aa0166069ef1a4bff1577979e9
vereis <me@vereis.com>
2026-03-10 12:37:04 +0000

    fix: expose crit as a flake app

--- flake.nix
+++ flake.nix
@@ -8,25 +8,32 @@
       version = "0.4.9";
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
-    in {
+    in rec {
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in {
           default = pkgs.buildGo126Module {
-          pname = "crit";
-          inherit version;
-          src = self;
-          vendorHash = null;
-          nativeCheckInputs = [ pkgs.git ];
-          ldflags = [ "-s" "-w" "-X main.version=${version}" ];
-          meta = with nixpkgs.lib; {
-            description = "Browser-based markdown review tool with inline commenting";
-            homepage = "https://github.com/tomasz-tomczyk/crit";
-            license = licenses.mit;
-            mainProgram = "crit";
-          };
+            pname = "crit";
+            inherit version;
+            src = self;
+            vendorHash = null;
+            nativeCheckInputs = [ pkgs.git ];
+            ldflags = [ "-s" "-w" "-X main.version=${version}" ];
+            meta = with nixpkgs.lib; {
+              description = "Browser-based markdown review tool with inline commenting";
+              homepage = "https://github.com/tomasz-tomczyk/crit";
+              license = licenses.mit;
+              mainProgram = "crit";
+            };
           };
         });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${packages.${system}.default}/bin/crit";
+        };
+      });
     };
 }
